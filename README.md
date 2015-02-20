9d76dbeb665fdbc9915014e0e114c12ad60406d7
s-a <stephan.ahlf@googlemail.com>
2015-02-20 22:42:40 +0100

    added option to convert a complete folder

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # video-datetime-stamp
 Render a date/time stamp into videos using ffmpeg
 
-Render tdate time stamps to videos with ffmpeg is annoying.
+Render date time stamps to videos with ffmpeg is annoying.
 This node js command line is an easement to do this job.
 
 
 This command creates an output file at c:/git/video-datetime-stamp/test.stamped.mp4  
 ```node ./bin/render-stamp.js file c:/git/video-datetime-stamp/test.mp4```
+
+This command will stamp every mp4 file in c:\video\  
+```node ./bin/render-stamp.js dir c:\video\```
--- bin/render-stamp.js
+++ bin/render-stamp.js
@@ -9,26 +9,22 @@ var shell = require('shelljs');
 program
   .version('0.0.1')
   .option('-f, --file', 'Render date time stamp to file')
+  .option('-d, --dir', 'Render date time stamp to files')
   .parse(process.argv);
 
-function getVideoDuration(done){
+function getVideoDuration(filename){
 	var exec = require('child_process').exec;
 	var child;
-	child = exec("ffmpeg -i " + filename + " 2>&1 | grep \"Duration\"| cut -d ' ' -f 4 | sed s/,//",
-	  function (error, stdout, stderr) {
-
-	    var duration = stdout.replace(/\n/g, "").split(":");
-	    var result = {
-	    	hours : parseInt(duration[0]),
-	    	minutes : parseInt(duration[1]),
-	    	seconds : parseInt(duration[2].split(".")[0]),
-	    	milliseconds : parseInt(duration[2].split(".")[1])
-	    };
-	    done(result);
-	    if (error !== null) {
-	      console.log('exec error: ' + error);
-	    }
-	});
+	var cmd = "ffmpeg -i " + filename + " 2>&1 | grep \"Duration\"| cut -d ' ' -f 4 | sed s/,//";
+	var duration =  (shell.exec(cmd).output.replace(/\n/g, "").replace(/\r/g, ""));
+    duration = duration.replace(/\n/g, "").split(":");
+    var result = {
+    	hours : parseInt(duration[0]),
+    	minutes : parseInt(duration[1]),
+    	seconds : parseInt(duration[2].split(".")[0]),
+    	milliseconds : parseInt(duration[2].split(".")[1])
+    };
+  	return result;
 }
 
 function logVideoTimeInfo(name, videoTime) {
@@ -44,8 +40,23 @@ function getVideoStartTime(videoEndTime, duration){
 	return result;
 }
 
-if (program.file)  {
-	var filename = program.file[0];
+function getFiles (dir, files_){
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files){
+        var name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()){
+            getFiles(name, files_);
+        } else {
+        	if (name.indexOf(".stamped")===-1 && path.extname(name).toLowerCase() === ".mp4"){
+            	files_.push(name);
+        	}
+        }
+    }
+    return files_;
+}
+
+function convertVideo(filename) {
 	var outputFilename = filename.split(".");
 	outputFilename.splice(outputFilename.length-1, 0, "stamped");
 	outputFilename = outputFilename.join(".");
@@ -61,27 +72,41 @@ if (program.file)  {
 
 	logVideoTimeInfo("video end", videoTime);
 	
-	getVideoDuration(function(duration) {
-		console.log("duration: ", duration);
-		var startTime = getVideoStartTime(videoTime, duration);
-		logVideoTimeInfo("video start", startTime);
-		var basetime =  startTime.toDate().getTime() + "000";
+	var duration = getVideoDuration(filename);
+	console.log("duration: ", duration);
+	
 
-		console.log("ffmpeg basetime:", basetime);
+	var startTime = getVideoStartTime(videoTime, duration);
+	logVideoTimeInfo("video start", startTime);
+	var basetime =  startTime.toDate().getTime() + "000";
 
-		var command = "ffmpeg";
-		var parms = [];
-		parms.push ("-i \"" + filename + "\"");
-		parms.push ("-f MP4");
-		parms.push ("-vf \"drawtext=expansion=strftime: fontfile=" + fontFilename + ": text='%a %d\\.%m\\.%Y / %H\\:%M\\:%S': x=10:y=10: fontcolor=white: box=1: boxcolor=0x00000000@1: basetime="+basetime+"\"");
-	 	//parms.push ("-t 24"); // test 24 seconds
-	 	parms.push ("-preset ultrafast");
-	 	 //ultrafast, superfast, veryfast, faster, fast, medium (the default), slow, slower, veryslow.
-	 	parms.push ("-y");
-	 	parms.push (outputFilename);
-		console.log("Start ", command + " " + parms.join(" "));
-		if (shell.exec(command + " " + parms.join(" ")).code === 0){
-			console.log("done.");
-		}
-	});
+	console.log("ffmpeg basetime:", basetime);
+
+	var command = "ffmpeg";
+	var parms = [];
+	parms.push ("-i \"" + filename + "\"");
+	parms.push ("-f MP4");
+	parms.push ("-vf \"drawtext=expansion=strftime: fontfile=" + fontFilename + ": text='%a %d\\.%m\\.%Y / %H\\:%M\\:%S': x=10:y=10: fontcolor=white: box=1: boxcolor=0x00000000@1: basetime="+basetime+"\"");
+ 	//parms.push ("-t 5"); // test 5 seconds
+ 	parms.push ("-preset ultrafast");
+ 	 //ultrafast, superfast, veryfast, faster, fast, medium (the default), slow, slower, veryslow.
+ 	parms.push ("-y");
+ 	parms.push (outputFilename);
+	console.log("Start ", command + " " + parms.join(" "));
+	if (shell.exec(command + " " + parms.join(" ")).code === 0){
+		console.log("done.");
+	}
+};
+
+if (program.file)  {
+	var filename = program.file[0];
+	convertVideo(filename);
+}
+
+if (program.dir)  {
+	var files = getFiles(program.dir[0]);
+	for (var i = 0; i < files.length; i++) {
+		var fn = files[i];
+		convertVideo(fn);
+	};
 }
